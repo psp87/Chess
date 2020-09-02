64e3ac7a9332b6f9fa2e78904c3e88535d11fa27
psp87 <psp87@abv.bg>
2020-09-02 21:54:36 +0300

    Add userManager. New Home Page image.

--- Web/Chess.Web/Controllers/GameController.cs
+++ Web/Chess.Web/Controllers/GameController.cs
@@ -1,23 +1,30 @@
 ﻿namespace Chess.Web.Controllers
 {
+    using Chess.Data.Models;
     using Chess.Services.Data.Contracts;
 
     using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Identity;
     using Microsoft.AspNetCore.Mvc;
+    using System.Threading.Tasks;
 
     [Authorize]
     public class GameController : BaseController
     {
         private readonly IGameService gameService;
+        private readonly UserManager<ApplicationUser> userManager;
 
-        public GameController(IGameService gameService)
+        public GameController(IGameService gameService, UserManager<ApplicationUser> userManager)
         {
             this.gameService = gameService;
+            this.userManager = userManager;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return this.View(this.User.Identity);
+            var user = await this.userManager.GetUserAsync(this.User);
+
+            return this.View(user);
         }
 
         public IActionResult New()
