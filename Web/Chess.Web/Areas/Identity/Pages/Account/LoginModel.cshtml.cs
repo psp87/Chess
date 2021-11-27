8b05920d474e4bcf17ead588e1b3f47be1ea8825
Plamen Plamen <pspetrov1987@gmail.com>
2021-11-27 12:08:42 +0200

    Fix Warnings

--- Web/Chess.Web/Areas/Identity/Pages/Account/LoginModel.cshtml.cs
+++ Web/Chess.Web/Areas/Identity/Pages/Account/LoginModel.cshtml.cs
@@ -37,20 +37,6 @@
         [TempData]
         public string ErrorMessage { get; set; }
 
-        public class InputModel
-        {
-            [Required]
-            [EmailAddress]
-            public string Email { get; set; }
-
-            [Required]
-            [DataType(DataType.Password)]
-            public string Password { get; set; }
-
-            [Display(Name = "Remember me?")]
-            public bool RememberMe { get; set; }
-        }
-
         public async Task OnGetAsync(string returnUrl = null)
         {
             if (!string.IsNullOrEmpty(this.ErrorMessage))
@@ -104,6 +90,20 @@
 
             // If we got this far, something failed, redisplay form
             return this.Page();
+        }
+
+        public class InputModel
+        {
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; }
+
+            [Required]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+
+            [Display(Name = "Remember me?")]
+            public bool RememberMe { get; set; }
         }
     }
 }
